9e8f26c383d7fa5774bec6d2efa034edadbe1a17
Rudrakrishna-Das <rudradas16996@gmail.com>
2023-04-28 15:51:43 +0530

    creating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import MealsSummary from "./components/Meals/MealsSummary/MealsSummary";
 
 function App() {
   return (
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,16 +4,23 @@ import HeaderCartButton from "./HeaderCartButton/HeaderCartButton";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import MealsSummary from "../Meals/MealsSummary/MealsSummary";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <h1 className={classes.website_name}>ReactMeals</h1>
-        <HeaderCartButton />
-      </nav>
-      <img src={mealsImage} alt="table full of foods" />
-    </header>
+    <React.Fragment>
+      <header>
+        <nav>
+          <h1 className={classes.website_name}>ReactMeals</h1>
+          <HeaderCartButton />
+        </nav>
+
+        <img src={mealsImage} alt="table full of foods" />
+      </header>
+      <main>
+        <MealsSummary />
+      </main>
+    </React.Fragment>
   );
 };
 export default Header;
