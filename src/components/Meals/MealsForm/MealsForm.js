62c9eac7cc8ab81c42b95c5bf7ccbc6efb282ed2
Rudrakrishna-Das <rudradas16996@gmail.com>
2023-05-09 10:32:54 +0530

    new feature added

--- src/components/Meals/MealsForm/MealsForm.js
+++ src/components/Meals/MealsForm/MealsForm.js
@@ -1,29 +1,60 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 import classes from "./MealsForm.module.css";
 
 const MealsForm = (props) => {
-  const addToCartValue = useRef();
+  // const addToCartValue = useRef();
+  const [cartValue, setCartValue] = useState(0);
+  const [decreseBtnIsDisabled, setDecreseBtnIsDisabled] = useState(true);
+  const [increseBtnIsDisabled, setIncreseBtnIsDisabled] = useState(false);
+
+  const increaseHandler = () => {
+    if (cartValue === 4) {
+      setCartValue((prevValue) => prevValue + 1);
+      setIncreseBtnIsDisabled(true);
+    } else {
+      setCartValue((prevValue) => prevValue + 1);
+      setDecreseBtnIsDisabled(false);
+    }
+  };
+
+  const decreaseHandler = () => {
+    if (cartValue === 5) {
+      setCartValue((prevValue) => prevValue - 1);
+      setIncreseBtnIsDisabled(false);
+    } else if (cartValue === 1) {
+      setCartValue((prevValue) => prevValue - 1);
+      setDecreseBtnIsDisabled(true);
+    } else {
+      setCartValue((prevValue) => prevValue - 1);
+    }
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    const cartValue = +addToCartValue.current.value;
+    // const cartValue = +addToCartValue.current.value;
 
     props.onAddToCart(cartValue);
   };
   return (
     <form className={classes.mealitem_form} onSubmit={submitHandler}>
       <Input
-        ref={addToCartValue}
-        label="CartValue"
+        // label="CartValue"
+        // ref={addToCartValue}
+        onIncreaseButton={increseBtnIsDisabled}
+        onDecreaseButton={decreseBtnIsDisabled}
+        onIncrease={increaseHandler}
+        onDecrease={decreaseHandler}
         input={{
           id: "amount_" + props.id,
           type: "number",
           min: 1,
           max: 5,
-          defaultValue: 0,
+          value: cartValue,
         }}
       />
+
       <div className={classes.action}>
         <button type="submit">Add To Cart</button>
       </div>
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,53 @@
 import React from "react";
 import classes from "./Input.module.css";
-
+/*
 const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.amount}>
-      <label htmlFor={`amount_1`}>{props.label}</label>
-      <input ref={ref} {...props.input} />
+      { <label htmlFor={`amount_1`}>{props.label}</label> }
+      <button
+        type="button"
+        className={classes.decrease}
+        onClick={props.onDecrease}
+      >
+        -
+      </button>
+      <input ref={ref} {...props.input} disabled />
+      <button
+        type="button"
+        className={classes.increase}
+        onClick={props.onIncrease}
+      >
+        +
+      </button>
     </div>
   );
 });
+*/
+const Input = (props) => {
+  // const increaseButton = props
+  return (
+    <div className={classes.amount}>
+      {/* <label htmlFor={`amount_1`}>{props.label}</label> */}
+      <button
+        type="button"
+        className={classes.decrease}
+        onClick={props.onDecrease}
+        disabled={props.onDecreaseButton}
+      >
+        -
+      </button>
+      <input {...props.input} disabled />
+      <button
+        type="button"
+        className={classes.increase}
+        onClick={props.onIncrease}
+        disabled={props.onIncreaseButton}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
 export default Input;
